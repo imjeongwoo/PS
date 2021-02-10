dc8d64e07dd3189cb0d06a7cc412169def75a7a4
imjeongwoo <limjeong1219@gmail.com>
2021-02-10 15:20:05 +0900

    2812번 크게만들기(그리디) - 간단한 풀이

--- cpp/2812크게만들기.cpp
+++ cpp/2812크게만들기.cpp
@@ -1,64 +1,30 @@
-// #include <bits/stdc++.h>
-
-// using namespace std;
-
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL); cout.tie(NULL);
-
-//     int N, K;
-//     string nums;
-//     deque<char> deq;
-
-//     cin >> N >> K >> nums;
-
-
-//     for (int i = 0; i < N; i++) {
-//         while (K && !deq.empty() && deq.back() < nums[i]) {
-//             K--;
-//             deq.pop_back();
-//         }
-
-//         deq.push_back(nums[i]);
-//     }
-    
-//     for (int i = 0; i < deq.size() - K; i++)
-//         cout << deq[i];
-//     cout << "\n";
-//     return 0;
-// }
-
-
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
 
+int main(void) {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    char result[500001];
     int N, K;
-    vector<int> nums;
-    deque<int> deq;
+    string S;
 
-    cin >> N >> K; 
-    for (int i = 0; i < N; i++) {
-        int num;
-        scanf("%1d", &num);
-        nums.push_back(num);
-    }
+    cin >> N >> K >> S;
+
+    int top = 0;
 
     for (int i = 0; i < N; i++) {
-        while (K > 0 && !deq.empty() && deq.back() < nums[i]) {
+        while (K > 0 && top > 0 && S[i] > result[top - 1]) {
+            top--;
             K--;
-            deq.pop_back();
         }
-
-        deq.push_back(nums[i]);
+        result[top++] = S[i];
     }
-    
-    for (int i = 0; i < deq.size() - K; i++)
-        cout << deq[i];
-    
+    result[top-K] = '\0';
+    cout << result;
+
     return 0;
 }
