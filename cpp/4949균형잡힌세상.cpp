be8fd17585eb74f820bf9709f54ed32f51844250
imjeongwoo <limjeong1219@gmail.com>
2021-02-25 15:52:45 +0900

    update

--- cpp/4949균형잡힌세상.cpp
+++ cpp/4949균형잡힌세상.cpp
@@ -8,23 +8,33 @@ int main(void) {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
     string str;
+    
 
-    while (getline(cin, str) && str != ".") {
+    while (true) {
+        bool isTrue = true;
+        getline(cin, str);
+        if (str == ".") break;
         stack<char> S;
         for (auto c : str) {
             if (c == '(' || c == '[') S.push(c);
             else if (c == ')') {
                 if (!S.empty() && S.top() == '(') S.pop();
-                else break;
+                else {
+                    isTrue = false;
+                    break;
+                }
             }
             else if (c == ']') {
                 if (!S.empty() && S.top() == '[') S.pop();
-                else break;
+                else {
+                    isTrue = false;
+                    break;
+                }
             }
         }
-        if (S.empty()) cout << "yes\n";
+        if (!S.empty()) isTrue = false;
+        if (isTrue) cout << "yes\n";
         else cout << "no\n";
     }
-    cout << "yes";
     return 0;
 }
