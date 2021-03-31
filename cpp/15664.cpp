1615e8edd49ad4c3830c753cbc4ff2eac04801bf
imjeongwoo <limjeong1219@gmail.com>
2021-04-01 02:21:47 +0900

    N과 M(10)

---
+++ cpp/15664.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, w;
+int arr[9];
+int result[9];
+int vis[9];
+int isused[100001];
+
+void dfs(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++)
+            cout << result[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    int pr = 0;
+    for (int i = 0; i < n; i++) {
+        if (vis[i] || w > arr[i]) continue;
+        if (pr == arr[i]) continue;
+        w = arr[i];
+        result[k] = arr[i];
+        pr = arr[i];
+        vis[i] = 1;
+        dfs(k + 1);
+        vis[i] = 0;
+        w = arr[i];
+    }
+}
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    dfs(0);
+    return 0;
+}
