95afac54f226f0973c7075e9b53793b9a0ffd66c
imjeongwoo <limjeong1219@gmail.com>
2021-04-10 22:33:44 +0900

    N과 M(11)

---
+++ cpp/15665.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[7];
+int vis[7];
+int result[7];
+
+void dfs(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++)
+            cout << result[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int pr = 0;
+    for (int i = 0; i < n; i++) {
+        if (pr != arr[i]) {
+            pr = arr[i];
+            result[k] = arr[i];
+            dfs(k + 1);
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    dfs(0);
+
+    return 0;
+}
