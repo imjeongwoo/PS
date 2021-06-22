7d64a7a5b1955a7a56166d3f11422fa87425fc22
imjeongwoo <limjeong1219@gmail.com>
2021-06-22 15:29:07 +0900

    예산

---
+++ programmers/Lv1/예산.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for(int i = 0; i < d.size(); i++){
+        if(budget - d[i] < 0) break;
+        answer++;
+        budget -= d[i];
+    }
+    return answer;
+}
